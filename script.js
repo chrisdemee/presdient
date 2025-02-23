937c8d3890ea4c72ba085b4de2d29bf6d5550346
chrisdemee <526cdemetri@frhsd.com>
2025-02-23 17:37:19 +0000

    random quote button generates

---
+++ script.js
@@ -0,0 +1,17 @@
+const nixonQuotes = [
+    "The greatest honor history can bestow is the title of peacemaker.",
+    "A man is not finished when he is defeated. He is finished when he quits.",
+    "Only if you have been in the deepest valley can you ever know how magnificent it is to be on the highest mountain.",
+    "If you want to make beautiful music, you must play the black and the white notes together.",
+    "The presidency has many problems, but boredom is the least of them."
+];
+
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * nixonQuotes.length);
+    document.getElementById("quoteDisplay").innerText = nixonQuotes[randomIndex];
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("quoteDisplay").innerText = nixonQuotes[0]; // show first quote when site loads
+    document.getElementById("generateQuote").addEventListener("click", getRandomQuote);
+});
